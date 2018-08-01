b58d237d7f690a67a689d6a7e57c7c9e152ae993
datranz <nicktran909@gmail.com>
2018-08-01 14:32:33 -0700

    lab2pt3

--- lab2pt3/lab2pt3/main.c
+++ lab2pt3/lab2pt3/main.c
@@ -1,29 +1,32 @@
 /*
- * lab2pt2.c
+ * lab2pt3.c
  *
- * Created: 8/1/2018 1:07:48 PM
+ * Created: 8/1/2018 2:09:13 PM
  * Author : ucrcse
  */ 
 
 #include <avr/io.h>
 // bit-access function
 unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b){
-	return ( b ? x |(0x01 <<k) : x & ~ (0x01 << k));}
-	
+return ( b ? x |(0x01 <<k) : x & ~ (0x01 << k));}
+
 
 
 
 int main(void)
 {
- DDRA=0x00;	PORTA=0xFF;
- DDRC=0xFF;	PORTB=0x00;
+	DDRA=0x00;	PORTA=0xFF;
+	DDRC=0xFF;	PORTB=0x00;
 
- unsigned char fuel=0x00;
- unsigned char led=0x00;
-  
-    while (1) 
-    {
+	unsigned char fuel=0x00;
+	unsigned char led=0x00;
+	unsigned char sbicon=0x00; 
+	
+	while (1)
+	{
 		fuel=PINA;
+		sbicon=fuel;
+		
 		for(unsigned char i=4; i<=7; i++){fuel = SetBit(fuel, i, 0);}
 		
 		led=0x00;
@@ -32,17 +35,11 @@ int main(void)
 		if(fuel>=5){led=SetBit(led,3,1);}
 		if(fuel>=7){led=SetBit(led,2,1);}
 		if(fuel>=10){led=SetBit(led,1,1);}
-		if(fuel>=13){led=SetBit(led,0,1);}	
+		if(fuel>=13){led=SetBit(led,0,1);}
 		if(fuel<=4){led=SetBit(led,6,1);}
+		if((sbicon & 0x10)&&(sbicon & 0x20)&&(sbicon & 0x40)){led=SetBit(led,7,1);}
 		
-		
-		PORTC=led;		
-    }
+		PORTC=led;
+	}
 }
-
-
-
-
-
-
 
